04885ad6e5de5263d2d59f080d6ec4f8a1f19d75
BAM-hub <bsharamin12@gmail.com>
2021-11-03 13:41:55 +0200

    combine components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { InputProvider } from './contexts/InputContex';
+import { ResultProvider } from './contexts/ResultContext';
+import Input from './components/input/Input';
+import Result from './components/result/Result';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <InputProvider>
+        <ResultProvider>
+          <Input />
+          <Result />
+        </ResultProvider>
+      </InputProvider>
     </div>
   );
-}
+};
 
 export default App;
