c5e760aa0d97f3c009a4c5b214b93501bef2ebcf
BAM-hub <bsharamin12@gmail.com>
2021-11-03 13:40:30 +0200

    simple text input component

---
+++ src/components/input/TextInput.js
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import { DataContext } from '../../contexts/DataContext';
+import { BinContext } from '../../contexts/BinContext';
+import { binByMean } from '../../utils/simpleData';
+import { ResultContext } from '../../contexts/ResultContext';
+
+const TextInput = () => {
+
+  const [data, setData] = useContext(DataContext);
+  const [binData, setBinData] = useContext(BinContext);
+  const [result, setResult] = useContext(ResultContext);
+
+  const { binType, binCount } = binData;
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    if(binType === 'bbm' ){
+      
+      setResult(binByMean(data, binCount));
+      //console.log(result);
+    } else {
+      console.log('yet');
+    }
+
+  };
+  return (
+    <form onSubmit={e => onSubmit(e)}>
+      <input
+        type='text'
+        placeholder='Enter Simple Data here'
+        value={data}
+        onChange={e => setData(e.target.value)}
+      />
+      <button type='submit'>Submit</button>
+    </form>
+  );
+}
+
+export default TextInput;
