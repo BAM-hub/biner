89cd2c0053077a532245fcac2472d0c6db87637a
BAM-hub <bsharamin12@gmail.com>
2021-11-06 21:35:55 +0200

    combine components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import './App.css';
 import { InputProvider } from './contexts/InputContex';
 import { ResultProvider } from './contexts/ResultContext';
-import Input from './components/input/Input';
-import Result from './components/result/Result';
-import CsvDownloader from './components/result/CsvDownloader';
+import Layout from './components/layout/Layout';
 
 const App = () => {
   return (
     <div className='container'>
       <InputProvider>
         <ResultProvider>
-          <Input />
-          <Result />
-          <CsvDownloader />
+          <Layout />
         </ResultProvider>
       </InputProvider>
     </div>
