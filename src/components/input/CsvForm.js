e456f287a262618e6cb07139599cc3347b383f0c
BAM-hub <bsharamin12@gmail.com>
2021-11-06 18:12:38 +0200

    now can take simple csv files and formats them

--- src/components/input/CsvForm.js
+++ src/components/input/CsvForm.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { CSVReader } from 'react-papaparse';
 
+import { formatData, formatResult } from '../../utils/format';
+import { binByMean, binByBoundaries } from '../../utils/simpleData';
+
+import { ResultContext } from '../../contexts/ResultContext';
+import { BinContext } from '../../contexts/BinContext';
+import { DataContext } from '../../contexts/DataContext';
+
 const CsvForm = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useContext(DataContext);
+  const [result, setResult] = useContext(ResultContext);
+  const [binData, setBinData] = useContext(BinContext);
+  
+  const { binType, binCount } = binData;
+  const handleDrop = (results) => {
+    //console.log(results.data);
+    setData(formatData(results.data));
+    console.log(data);
+    if(binType === 'bbm') {
+      setResult(binByMean(data, binCount)); 
+    } else {
+      setResult(formatResult(binByBoundaries(data, binCount)));
+    }
 
-
+  }
   return (
     <div className="drop">
       <CSVReader
@@ -14,8 +34,7 @@ const CsvForm = () => {
           header: true,
           skipEmptyLines: true,
           complete: function (results) {
-            setData(results.data);
-            console.log(data);
+            handleDrop(results);
           }
         }}
       >
